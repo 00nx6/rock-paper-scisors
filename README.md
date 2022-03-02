d2a5e24095830c11008fce5be4531db6d958d50b
00nx6 <lszabo958@gmail.com>
2022-03-03 00:55:19 +0100

    Commit

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# rock-paper-scisors
+## rock-paper-scisors
 
 Simple rock papers scisors game i made as a warm up to getting back into javascript
+
+
+# aim
+
+1. the user should be able to select their play option through a GUI rather than a console input
+2. Three buttons, 1 for each option.
+3. Add a score board to see who won/lost
+4. Add a simple rules section
+
+
+
+# pseudocode
+create an options list with rock paper scisors in ther
+assign compChoice to choice[random number generator]
+
+make a variable called player choice
+make a variable called btns with a that gets all the children of playCont
+    add event listeners to all buttons through a forEach function that runs assignment function
+    based on clicked bttn, assign playerchoice to be data-key 
+}
+
+create an object that stores winner information
+create a pop up component that displays winner information of current round
+
+make an if statement to determine who wins that is run when a button is clicked
+>myb make a scoreboard at the bottom of the page (thats why the object)
--- script.js
+++ script.js
@@ -1,51 +1,74 @@
 
-/* 
-from choice of computer play, find the index of the choice in the list
+// options that can be played
+const choice = ['rock', 'paper', 'scisors'];
 
-in playround, find indexof playerchoice in playOptions
-
-make an if statement, where according to the index of the choice, they win loose or tie.
-
-*/
-
-
-const choice = ["rock", "paper", "scissors"]
-
-function compPlay() {
-    const n = Math.floor(Math.random() * 3);
-    console.log("The computer picked : " + choice[n])
-    return n;
+// initializing variables so they can be used globally
+let compChoice = choice[Math.floor(Math.random()*3)];
+let playerChoice;
+const body = document.querySelector('body');
+const container = document.createElement('section')
+const display = document.createElement('div');
+const resultCont = document.createElement('div');
+const flavourText = document.createElement('h2')
+const close = document.createElement('button');
+const result = document.createElement('h1');
+// function for assigning player choice and some style.
+function assignment() {
+    playerChoice = this.dataset.key
+    // fancy click animation
+    this.classList.remove('unClicked')
+    this.classList.add('clicked')
+    // gives game the choices
+    game(playerChoice, compChoice)
 }
-function playerPlay() {
-    const playerChoice = prompt("rock, paper, or scissors?")
-    const i = choice.indexOf(playerChoice)
-    return i
+function removeGlow() {
+    // reverses the glow affect on clock instead of just cutting it off
+    this.classList.remove('clicked')
+    this.classList.add('unClicked')
 }
-function play(i, n) {
-    if (i == n) {
-        console.log("its a tie")
-    } else if ( i == 0) {
-        if (n == 1) {
-            console.log("You loose!");
-        } else {
-            console.log("you win")
-        }
-    } else if ( i == 1) {
-        if (n == 0) {
-            console.log("you win")
-        } else {
-            console.log("you loose")
-        }
-    } else {
-        if (n == 0) {
-            console.log("you loose");
-        } else {
-            console.log("you win")
-        }
-    }
-    const replay = prompt("wanna play again? (Y/n)")
-    if (replay == "y") {
-        play(playerPlay(), compPlay())
-    }
+// assigns the buttons 
+const btns = Array.from(document.querySelector('.playCont').children)
+
+btns.forEach(button => {
+    button.addEventListener('click', assignment)
+    button.addEventListener('transitionend', removeGlow)
+})
+// decides the winner
+function game(player, computer) {
+
+
+    
+    popup(player, computer)
 }
-play(playerPlay(), compPlay())
+function popup(winner, message) {
+    // Creates the initial popup to let the player know if they've won or lost
+
+
+    container.classList.add('jsContainer', 'close');
+    resultCont.classList.add('jsResultCont')
+    flavourText.classList.add('jsFlavourText');
+    display.classList.add('jsDisplayResult');
+    close.classList.add('jsCloseBttn', 'close');
+    result.classList.add('result');
+    close.innerText = "Close"
+
+    // winner will be displayed
+    flavourText.innerText = `wagwan for batty man :kreyGasm:`
+    result.innerText = `${winner}`
+    resultCont.append(close)
+    resultCont.append(flavourText)
+    resultCont.append(result)
+    
+    display.append(resultCont)
+    container.append(display)
+    body.append(container)
+    
+    container.addEventListener('click', closePopup)
+    close.addEventListener('click', closePopup);
+}
+
+// popup for the close bttn
+function closePopup(e) {
+    if (e.target.classList.contains('close')) container.parentNode.removeChild(container);
+}   
+ 
